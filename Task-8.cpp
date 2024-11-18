bbb9e49d296e9ad2186faff3fef9edc205955727
Artem-14 <76739728+Artem-14@users.noreply.github.com>
2024-11-19 01:32:10 +0300

    Add files via upload

---
+++ Task-8.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b, c;
+
+    cout << "Введите сумму вклада: ";
+    cin >> a;
+
+    cout << "Введите годовую процентную ставку: ";
+    cin >> b;
+
+    c = (a * (b / 100)) / 12;
+
+    cout << "Банк будет платить " << c << " евро в месяц." << endl;
+    return 0;
+}
