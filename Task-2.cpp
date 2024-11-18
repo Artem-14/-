02968ab0e1e7be8748278a3e56e5e67d4caebea3
Artem-14 <76739728+Artem-14@users.noreply.github.com>
2024-11-18 20:42:37 +0300

    Add files via upload

---
+++ Task-2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    double L, R, S;
+    double pi = 3.14159265359;
+
+    cout << "Введите длину окружности круга: ";
+    cin >> L;
+
+    R = L / (2 * pi);
+    S = pi * R * R;
+
+    cout << "Площадь круга равна: " << S << endl;
+    return 0;
+}
+
+
