50e450f09dae4fb83e590e1fd7b045ac626ba642
Artem-14 <76739728+Artem-14@users.noreply.github.com>
2024-11-19 00:04:00 +0300

    Add files via upload

---
+++ Task-3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    double v, t, a, S;
+
+    cout << "Введите скорость: ";
+    cin >> v;
+    cout << "Введите время: ";
+    cin >> t;
+    cout << "Введите ускорение: ";
+    cin >> a;
+
+    S = v * t + (a * t * t) / 2;
+
+    cout << "Пройденное расстояние составляет: " << S << " метров" << endl;
+    return 0;
+}
+
