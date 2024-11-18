709ee554e1f5d5e9cdd698c1c2fd094a61c521b3
Artem-14 <76739728+Artem-14@users.noreply.github.com>
2024-11-19 00:46:16 +0300

    Add files via upload

---
+++ Task-5.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    double doll, cent;
+    int a;
+
+    cout << "Введите число: ";
+    cin >> doll;
+
+    a = doll;
+    cent = (doll - a) * 100;
+ 
+    cout << a << " долларов и " << cent << " центов" << endl;
+    return 0;
+
+}
+
