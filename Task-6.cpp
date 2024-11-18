d6042068cf7c6a7759c803d5618d98a3368322c6
Artem-14 <76739728+Artem-14@users.noreply.github.com>
2024-11-19 00:53:05 +0300

    Add files via upload

---
+++ Task-6.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int day;
+
+    cout << "Введите количество дней: ";
+    cin >> day;
+
+    int week = day / 7;
+    int day1 = day % 7;
+
+    cout << week << " недель и " << day1 << " дней" << endl;
+    return 0;
+}
