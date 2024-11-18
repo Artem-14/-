95d2c0312231457a83ffa9c3dcedc8e02aad7332
Artem-14 <76739728+Artem-14@users.noreply.github.com>
2024-11-19 01:22:07 +0300

    Add files via upload

---
+++ Task-7.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double distance, timemin, timesec;
+
+    cout << "Введите длину расстояния: ";
+    cin >> distance;
+    cout << "Введите время (например 3.25): ";
+    cin >> timemin;
+
+    timesec = (timemin - static_cast<int>(timemin)) * 100;
+    timemin = static_cast<int>(timemin);
+
+    double totaltime = timemin * 60 + timesec;
+    double speedm = distance / totaltime;
+    double speedkm = speedm * 3.6;
+
+    cout << "Расстояние: " << distance << " м" << endl;
+    cout << "Время: " << timemin << " мин " << timesec << " сек = " << totaltime << " секунд" << endl;
+    cout << "Ты бежал на скорости " << fixed << setprecision(2) << speedkm << " км/ч" << endl;
+
+    return 0;
+}
