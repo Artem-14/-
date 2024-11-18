8ebfdb985dbaa6522ff7839080b8163a4f7b1657
Artem-14 <76739728+Artem-14@users.noreply.github.com>
2024-11-19 00:31:36 +0300

    Add files via upload

---
+++ Task-4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    int seconds;
+
+    cout << "Введите время в секундах: ";
+    cin >> seconds;
+
+    int hours = seconds / 3600;
+    seconds = seconds % 3600;
+    int minutes = seconds / 60;
+    seconds = seconds % 60;
+
+    cout << hours << " часов, " << minutes << " минут, " << seconds << " секунд" << endl;
+    return 0;
+
+}
+
