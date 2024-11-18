081f03f174c2eb52089ff2bd36ab628d0c3d4060
Artem-14 <76739728+Artem-14@users.noreply.github.com>
2024-11-18 20:29:23 +0300

    Add files via upload

---
+++ Task-1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    double R1, R2, R3, R0;
+    cout << "Введите R1: ";
+    cin >> R1;
+    cout << "Введите R2: ";
+    cin >> R2;
+    cout << "Введите R3: ";
+    cin >> R1 >> R2 >> R3;
+
+
+    R0 = 1 / (1 / R1 + 1 / R2 + 1 / R3);
+
+    cout << "Полное сопротивление R0 равно: " << R0 << endl;
+    return 0;
+}
